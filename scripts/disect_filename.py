b3f6bb6a6e29369f277461e1905e34c264fdc2cc
root <root>
2012-01-02 17:37:08 +0000

    -moving

---
+++ scripts/disect_filename.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+def disectFileName(fileName):
+
+  #get base file name
+  parts0=fileName.split('[',1)
+  if len(parts0)<2:# maybe a single file?
+    parts3=fileName.split('_t')
+    if parts3[1].isdigit():
+      start=int(parts3[1])
+      end=start+1
+      baseFileName=parts3[0]+'_t'
+    else:
+      print "unrecognized file type expecting something with a _tXXXXXXXX suffix, where X's denote digits."
+      quit()
+  
+  else:# a range of files
+    baseFileName=parts0[0]
+    
+    #get interation range
+    parts1=parts0[1].split(']')
+    parts2=parts1[0].split('-')
+    start=int(parts2[0])
+    if parts2[1]=="*":
+      end=sys.maxint
+    else:
+      end=int(parts2[1])
+  
+  return [start,end,baseFileName]
