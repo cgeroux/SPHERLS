59a435b144c13ad9c49d76836d8808417cb35680
cgeroux <chris.m.geroux@gmail.com>
2012-05-29 11:40:10 +0000

    -didn't properly resolve a conflict. It is now.

--- scripts/paths.py
+++ scripts/paths.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python
 import os
-<<<<<<< .working
-BasePath="/home/cgeroux/SPHERLS/"
-SPHERLSanalPath=BasePath+"SPHERLSanal"
-SPHERLSgenPath=BasePath+"SPHERLSgen"
-SPHERLSPath=BasePath+"SPHERLS"
-scriptPaths=BasePath+"scripts/"
-SPHERLSDATA=BasePath+"data/"
-EOSPath=BasePath+"eosY240Z002"
-velocityProfilePath=BasePath+"data/velocity_pro/T5700_L50_M575_fu.dat"
-ref_calcs=BasePath+"data/ref_calcs/"
-=======
 import sys
->>>>>>> .merge-right.r1063
 
-<<<<<<< .working
-=======
 BasePath=os.path.dirname(os.path.dirname(sys.argv[0]))#remove script name, remove scripts directory
 SPHERLSanalPath=BasePath+"/SPHERLSanal"
 SPHERLSgenPath=BasePath+"/SPHERLSgen"
@@ -23,9 +9,8 @@ SPHERLSPath=BasePath+"/SPHERLS"
 scriptPaths=BasePath+"/scripts/"
 SPHERLSDATA=BasePath+"/data/"
 EOSPath=BasePath+"/data/eos/eosY240Z002"
-velocityProfilePath=BasePath+"/data/velocity_pro/T5700_L50_M575_fu.dat"
+velocityProfilePath=BasePath+"/data/velocity_pro/"
 ref_calcs=BasePath+"/data/ref_calcs/"
->>>>>>> .merge-right.r1063
 
 def check_paths():
   #check paths
